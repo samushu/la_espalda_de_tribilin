d3715b0935881765562b6b7a3d156f6a0ac0e008
samusshu <sgaponte@uniboyaca.edu.co>
2026-06-08 18:16:39 -0500

    samuel: [Arreglo del index en ms_seguimiento]

--- ms_seguimiento/Public/Index.php
+++ ms_seguimiento/Public/Index.php
@@ -1,13 +1,13 @@
 <?php
 use Slim\Factory\AppFactory;
-use App\seguimiento\Modelos\seguimiento;
+use App\seguimiento\Modelo\Seguimiento;
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../App/Confi/ConDB.php';
 
 $cors = require __DIR__ . '/../Middlewars/CorsMiddlewar.php';
 
-$incapacidadesEndpoints   = require __DIR__ . '/../App/incapacidades/Presentacion/Routers/Endpoints.php';
+$incapacidadesEndpoints   = require __DIR__ . '/../App/seguimiento/Presentacion/Routers/Endpoints.php';
 
 $app = AppFactory::create();
 
